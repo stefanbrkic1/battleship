23d08deacf63c81515b6c09a11d3ea094b7cebd8
stefanbrkic1 <stefanbrkicdzn.contact@gmail.com>
2023-11-20 14:18:33 +0100

    Added battleship HTML page

---
+++ battleship.2c1c2ea3a11d6d2ec43a.js
@@ -0,0 +1,18 @@
+(self["webpackChunkbattleship"] = self["webpackChunkbattleship"] || []).push([["battleship"],{
+
+/***/ "./src/battleship.js":
+/*!***************************!*\
+  !*** ./src/battleship.js ***!
+  \***************************/
+/***/ (() => {
+
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/battleship.js"));
+/******/ }
+]);
